bb8667d844f1e21ee3ead5c8884840df66cb6530
cyrusfirheir <56257647+cyrusfirheir@users.noreply.github.com>
2020-12-02 11:09:57 +0530

    multiple ctp instances on same page bugfix

--- click-to-proceed/click-to-proceed.js
+++ click-to-proceed/click-to-proceed.js
@@ -186,7 +186,9 @@
 			});
 			variables()["#macro-ctp-dump"] = variables()["#macro-ctp-dump"] || {};
 			variables()["#macro-ctp-dump"][_id] = ctp;
-			$(this.output).wiki(ctp.out()).find(".macro-ctp-wrapper").attr("data-ctp", Util.escape(_id)).find(".ctp-head").wiki(CTP.item(ctp.head)).siblings(".ctp-tail").wiki(CTP.item(ctp.tail));
+			let out = $("<div />");
+			out.wiki(ctp.out()).find(".macro-ctp-wrapper").attr("data-ctp", Util.escape(_id)).find(".ctp-head").wiki(CTP.item(ctp.head)).siblings(".ctp-tail").wiki(CTP.item(ctp.tail));
+			$(this.output).append(out.children().unwrap());
 		}
 	});
 
