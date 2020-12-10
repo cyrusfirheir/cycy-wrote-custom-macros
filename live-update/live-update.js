a1bb71de8d241ca4bccee48d369b2ba131ba2890
cyrusfirheir <56257647+cyrusfirheir@users.noreply.github.com>
2020-12-10 21:07:53 +0530

    live-update shadow wrapper fix

--- live-update/live-update.js
+++ live-update/live-update.js
@@ -23,10 +23,10 @@
 				if (result !== null) {
 					var lh = this.name === "lh";
 					var $el = $("<span></span>").addClass("macro-live").wiki(lh ? Util.escape(result) : result).appendTo(this.output);
-					$el.on(":liveupdateinternal", function (ev) {
+					$el.on(":liveupdateinternal", this.createShadowWrapper(function (ev) {
 						var out = toStringOrDefault(Scripting.evalJavaScript(statement), null);
 						$el.empty().wiki(lh ? Util.escape(out) : out);
-					});
+					}));
 				}
 			} catch (ex) {
 				return this.error("bad evaluation: " + (_typeof(ex) === 'object' ? ex.message : ex));
@@ -41,9 +41,9 @@
 				var content = this.payload[0].contents.trim();
 				if (content) {
 					var $el = $("<span></span>").addClass("macro-live macro-live-block").wiki(content).appendTo(this.output);
-					$el.on(":liveupdateinternal", function (ev) {
+					$el.on(":liveupdateinternal", this.createShadowWrapper(function (ev) {
 						$el.empty().wiki(content);
-					});
+					}));
 				}
 			} catch (ex) {
 				return this.error("bad evaluation: " + (_typeof(ex) === 'object' ? ex.message : ex));
