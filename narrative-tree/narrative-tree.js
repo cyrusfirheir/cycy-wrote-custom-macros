5134a96a27b01c0ff7f8b4d2a0f2264b3c265a41
cyrusfirheir <56257647+cyrusfirheir@users.noreply.github.com>
2021-08-05 13:36:12 +0530

    NTree deltaVal parsing tweak

--- narrative-tree/narrative-tree.js
+++ narrative-tree/narrative-tree.js
@@ -9,11 +9,11 @@
 
 	function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-	function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+	function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
 
 	function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 
-	function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() { }; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+	function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() { }; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
 
 	function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
@@ -40,6 +40,11 @@
 			});
 		}
 		_createClass(NTree, [{
+			key: "state",
+			get: function get() {
+				return NTree.getState(this.id);
+			}
+		}, {
 			key: "registerHandler",
 			value: function registerHandler(id, def) {
 				var IDs = id instanceof Array ? id : [id];
@@ -82,7 +87,7 @@
 					function clear() {
 						if (handler.onClear) handler.onClear(macroContext);
 					}
-					if (delta[id]) {
+					if (delta.hasOwnProperty(id)) {
 						var deltaVal = delta[id];
 						var clearDirective = deltaVal === NTree.clear;
 						if (clearDirective) return clear();
@@ -99,12 +104,19 @@
 					}
 				});
 			}
-		}, {
-			key: "state",
-			get: function get() {
-				return NTree.getState(this.id);
-			}
 		}], [{
+			key: "Repository",
+			get: function get() {
+				if (!setup["@NTree/Repository"]) setup["@NTree/Repository"] = new Map();
+				return setup["@NTree/Repository"];
+			}
+		}, {
+			key: "StateStore",
+			get: function get() {
+				if (!variables()["@NTree/StateStore"]) variables()["@NTree/StateStore"] = new Map();
+				return variables()["@NTree/StateStore"];
+			}
+		}, {
 			key: "getNTree",
 			value: function getNTree(id) {
 				return NTree.Repository.get(id);
@@ -127,18 +139,6 @@
 			key: "deleteNTree",
 			value: function deleteNTree(id) {
 				return NTree.Repository.delete(id) && NTree.StateStore.delete(id);
-			}
-		}, {
-			key: "Repository",
-			get: function get() {
-				if (!setup["@NTree/Repository"]) setup["@NTree/Repository"] = new Map();
-				return setup["@NTree/Repository"];
-			}
-		}, {
-			key: "StateStore",
-			get: function get() {
-				if (!variables()["@NTree/StateStore"]) variables()["@NTree/StateStore"] = new Map();
-				return variables()["@NTree/StateStore"];
 			}
 		}]);
 		return NTree;
